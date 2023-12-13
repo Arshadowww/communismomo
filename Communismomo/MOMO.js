4170121142d29e60949f6b40ce7b2ac08970eca7
Arshadowww <153129985+Arshadowww@users.noreply.github.com>
2023-12-13 16:15:47 +0100

    Update MOMO.js

--- Communismomo/MOMO.js
+++ Communismomo/MOMO.js
@@ -1,21 +1,48 @@
-function add_line() {
-    var line = document.createElement("audio");
-    var head=document.getElementsByTagName('body')[0];
-    line.type = "audio/mp3";
-    line.src="";
-    line.id="bgSong" ;
- line.autoplay = true;
- line.loop = true;
-    head.appendChild(line);
+var nbmusic, day, x, y, z, midi, midi_title;
+
+nbmusic = 6; // mettez ici le nombre de musiques de votre liste de lecture
+day = new Date();
+z = day.getTime();
+y = (z - (parseInt(z/1000,10) * 1000))/10;
+x = parseInt(y/100*nbmusic,10) + 1; // ici on tire une musique au sort
+
+// insérez en dessous chacune des musiques avec leur titre :
+if (x == (1))
+{
+	midi="musiques/musique_printemps.mp3";
+	midi_title="Belle musique de printemps !";
 }
 
-if(document.getElementById('bgSong')==null){
- add_line();
-var audio = document.getElementById('bgSong');
-audio.volume = 0.5;
+if (x == (2))
+{
+	midi="musiques/musique_ete.mp3";
+	midi_title="Summer Dance";
 }
 
-var audio = document.getElementById('bgSong');
-audio.src='audio/goofy.mp3';
-audio.load();
-audio.play();
+if (x == (3))
+{
+	midi="musiques/musique_automne.mp3";
+	midi_title="Automne";
+}
+
+if (x == (4))
+{
+	midi="musiques/musique_hiver.mp3";
+	midi_title="La neige tombe en hiver";
+}
+
+if (x == (5))
+{
+	midi="musiques/musique_paques.mp3";
+	midi_title="Ecoute les cloches";
+}
+
+if (x == (6))
+{
+	midi="musiques/musique_noel.mp3";
+	midi_title="Petit papa noël";
+}
+
+document.write('<embed type="audio/x-mpeg3" src= ' + midi + ' autostart="true" loop="true" ')
+document.write('volume="100" align="center" width="70" height="25">')
+document.write('<p style="text-align:center;">Titre :  ' + midi_title + ' </p> ')
